5213ed95c5cba13a5237ef99dc0f68e0004441da
SushiGirrl <lykkeflor@gmail.com>
2023-11-07 15:43:41 +0100

    Basic giphy clone

--- main.js
+++ main.js
@@ -9,6 +9,42 @@ you are going to solve the problem BEFORE you start coding
  */
 //Pseudocode
 //Fetch the correct data
+let userInput = "";
+const inputElement = document.querySelector("input");
+
+//eventlistener that saves the new user input in a userInput variable
+inputElement.addEventListener("input", function() {
+    userInput = inputElement.value;
+    console.log(userInput);
+})
 //create function that renders the gifs to the html
-//Create search function, that activates when button is clicked
-//the search function needs an input
+
+const button = document.querySelector(".search-button");
+button.addEventListener("click", function (){
+
+    //console.log(userInput);
+    //fetches gif data determined by user input
+    fetch(`https://api.giphy.com/v1/gifs/search/?api_key=LGWEJK2qiTc7IAOSadGjJuq8P3cEvsdk&q=${userInput}`)
+        .then(response => response.json())
+        .then(gifData => {
+            //console.log(gifData.data[0].images.original.url);
+            renderGif(gifData);
+        });
+})
+
+
+function renderGif(gifData){
+
+    const ul = document.querySelector('ul');
+    ul.innerHTML = "";
+    let index = -1;
+
+    gifData.data.forEach(function (gif){
+        const li = document.createElement('li');
+        const image = document.createElement("img");
+        ul.appendChild(li);
+        li.appendChild(image);
+        index = index + 1;
+        image.src = gifData.data[index].images.original.url;
+    })
+}
