374cae28adbcbb679c394963dd0470b5693d31c4
SushiGirrl <lykkeflor@gmail.com>
2023-11-07 12:52:26 +0100

    Initial commit Exercise description Comments

---
+++ main.js
@@ -0,0 +1,14 @@
+//Exercise description
+/*
+Here is how it is going to work:
+The user can write some text indicating the gif he is looking for,
+click a button and then a gif will be found (using the searched word)
+and the gif will be displayed to the user.
+Try break this problem into smaller problems and write down how
+you are going to solve the problem BEFORE you start coding
+ */
+//Pseudocode
+//Fetch the correct data
+//create function that renders the gifs to the html
+//Create search function, that activates when button is clicked
+//the search function needs an input
